f52a1f36aaec008d09187e1e43625a566f4e8150
James Stine <james.stine@okstate.edu>
2025-10-01 14:03:08 -0500

    fix boo boo in osu89

--- Python/roots/osu89.py
+++ Python/roots/osu89.py
@@ -1,4 +1,4 @@
-from book import bisect
+from bisect1 import bisect1
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -7,7 +7,7 @@ def f(xp):
     return np.log(xp**2)-0.3
 
 
-(xp, fm, ea, iter) = bisect(f, -2.5, 0.5)
+(xp, fm, ea, iter) = bisect1(f, -2.5, 0.5)
 print(f(xp))
 print('xp = {0:7.8f}'.format(xp))
 print('function value = {0:7.3g}'.format(fm))

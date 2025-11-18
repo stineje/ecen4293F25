9ea0736a66dfc8f60ae0195fdccb2c2c27c73f4f
James Stine <james.stine@okstate.edu>
2025-11-18 17:37:16 -0600

    update book ref

--- Python/matrix/gaussian_elim_ex.py
+++ Python/matrix/gaussian_elim_ex.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from book import gaussnaive
+from gaussnaive import gaussnaive
 
 A = np.matrix('3, -0.1, -0.2; 0.1, 7.0, -0.3; 0.3, -0.2, 10.0')
 b = np.matrix('7.85; -19.3; 71.4')
